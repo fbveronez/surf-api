d629cbea1ee19e37376e9f33368d669d79177f27
Fernando Veronez <fernandoveronez_@hotmail.com>
2021-08-12 20:55:25 -0300

    feat: implements HTTPClient

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,25 +1,29 @@
-import {StormGlass} from '@src/clients/stormGlass';
-import axios from 'axios';
-import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours.json'
+import { StormGlass } from '@src/clients/stormGlass';
+import * as HTTPUtil from '@src/util/request';
+import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours.json';
 import stormGlassNormalized3HoursFixtures from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
-jest.mock('axios');
+jest.mock('@src/util/request');
 
 describe('Stormglass client', () => {
-  const mockedAxios = axios as jest.Mocked<typeof axios>
+  const MockedRequestClass = HTTPUtil.Request as jest.Mocked<
+    typeof HTTPUtil.Request
+  >;
+  const mockedRequest = new HTTPUtil.Request() as jest.Mocked<HTTPUtil.Request>;
 
   it('should return the normalized forecast from the Stormglass service', async () => {
     const lat = -33.79;
     const lng = 151.28;
 
-    mockedAxios.get.mockResolvedValue({data: stormGlassWeather3HoursFixtures});
+    mockedRequest.get.mockResolvedValue({
+      data: stormGlassWeather3HoursFixtures,
+    } as HTTPUtil.Response);
 
-    const stormglass = new StormGlass(mockedAxios);
+    const stormglass = new StormGlass(mockedRequest);
     const response = await stormglass.fetchPoints(lat, lng);
 
     expect(response).toEqual(stormGlassNormalized3HoursFixtures);
-
-  })
+  });
 
   it('should exclude incomplete data points', async () => {
     const lat = -33.79;
@@ -34,47 +38,47 @@ describe('Stormglass client', () => {
           time: '2020-04-12T00:00:00+00:00',
         },
       ],
-    }
+    };
 
-    mockedAxios.get.mockResolvedValue({data: incompleteResponde});
+    mockedRequest.get.mockResolvedValue({
+      data: incompleteResponde,
+    } as HTTPUtil.Response);
 
-    const stormGlass = new StormGlass(mockedAxios);
+    const stormGlass = new StormGlass(mockedRequest);
     const response = await stormGlass.fetchPoints(lat, lng);
 
     expect(response).toEqual([]);
-  })
+  });
 
   it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
     const lat = -33.79;
     const lng = 151.28;
 
-    mockedAxios.get.mockRejectedValue({message: 'Network Error'});
+    mockedRequest.get.mockRejectedValue({ message: 'Network Error' });
 
-    const stormglass = new StormGlass(mockedAxios);
+    const stormglass = new StormGlass(mockedRequest);
 
     await expect(stormglass.fetchPoints(lat, lng)).rejects.toThrow(
       'Unexpected error when trying to comunicate to StormGlass: Network Error'
     );
-  })
+  });
 
   it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
     const lat = -33.79;
     const lng = 151.28;
 
-    mockedAxios.get.mockRejectedValue({
+    MockedRequestClass.isRequestError.mockReturnValue(true);
+    mockedRequest.get.mockRejectedValue({
       response: {
         status: 429,
-        data: {errors: ['Rate Limit reached']},
+        data: { errors: ['Rate Limit reached'] },
       },
     });
 
-    const stormglass = new StormGlass(mockedAxios);
+    const stormglass = new StormGlass(mockedRequest);
 
     await expect(stormglass.fetchPoints(lat, lng)).rejects.toThrow(
-      'Unexpected error return by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429',
+      'Unexpected error return by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
     );
-  })
-
-
-})
-
+  });
+});
--- src/util/errors/internal-error.ts
+++ src/util/errors/internal-error.ts
@@ -1,5 +1,9 @@
 export class InternalError extends Error {
-  constructor(public message: string, protected code: 500, protected description?: string) {
+  constructor(
+    public message: string,
+    protected code?: 500,
+    protected description?: string
+  ) {
     super(message);
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
