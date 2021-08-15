12b8c2b52f36964fc29ad37f85963a26eabd1b95
Fernando Veronez <fernandoveronez_@hotmail.com>
2021-08-15 19:23:12 -0300

    feat: improve functional tests and add errors

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,7 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches Functional test', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
   describe('When creating a beach ', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -14,6 +17,25 @@ describe('Beaches Functional test', () => {
       expect(status).toBe(201);
 
       expect(body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const { body, status } = await global.testRequest
+        .post('/beaches')
+        .send(newBeach);
+      expect(status).toBe(422);
+
+      expect(body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
     });
   });
 });
