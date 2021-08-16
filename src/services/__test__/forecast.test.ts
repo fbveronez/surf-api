6606b55555770cf18d90ab7129245be60efba1ec
Fernando Veronez <fernandoveronez_@hotmail.com>
2021-08-16 09:22:00 -0300

    feat: implements end2end integration

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,11 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
-import {
-  Beach,
-  BeachPosition,
-  Forecast,
-  ForecastProcessingInternalError,
-} from '../forecast';
+import { Forecast, ForecastProcessingInternalError } from '../forecast';
+import { Beach, BeachPosition } from '@src/models/beach';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -18,7 +14,6 @@ describe('Forecast Service', () => {
       lng: 151.289824,
       name: 'Manly',
       position: BeachPosition.E,
-      user: 'some-id',
     },
   ];
 
