85fd4aedb1dc0331de6844da6dbca7f18357cd10
Fernando Veronez <fernandoveronez_@hotmail.com>
2021-08-18 09:19:47 -0300

    feat: implements json web token

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -60,4 +60,45 @@ describe('Users functional tests', () => {
       });
     });
   });
+  describe('When authenticating a user', () => {
+    it('should generate a token for a valid user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+
+      await new User(newUser).save();
+      const { body } = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: newUser.password });
+
+      expect(body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
+
+    it('should return UNAUTHORIZED if the user with the given email is not found', async () => {
+      const { status, body } = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: 'some-email@mail.com', password: '1234' });
+
+      expect(status).toBe(401);
+    });
+
+    it('should return UNAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@mail.com',
+        password: '1234',
+      };
+
+      await new User(newUser).save();
+      const { status } = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: 'different password' });
+
+      expect(status).toBe(401);
+    });
+  });
 });
