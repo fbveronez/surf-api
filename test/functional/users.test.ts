970bd4d3ab38effa57bd3b206a79e877129e27ad
Fernando Veronez <fernandoveronez_@hotmail.com>
2021-08-17 21:58:57 -0300

    feat: implements encrypted password

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,22 +1,28 @@
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
     await User.deleteMany();
   });
   describe('When creating a new user', () => {
-    it('should succesfully create a new user', async () => {
+    it('should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@mail.com',
         password: '1234',
       };
-
-      const { status, body } = await global.testRequest
-        .post('/users')
-        .send(newUser);
-      expect(status).toBe(201);
-      expect(body).toEqual(expect.objectContaining(newUser));
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+      await expect(
+        AuthService.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it('should return 400 when there is a validation error', async () => {
