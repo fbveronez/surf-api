2a843f106ddca19787607073cfe5d2bc40e14386
Fernando Veronez <fernandoveronez_@hotmail.com>
2021-08-18 21:38:13 -0300

    feat: implement auth middleware

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,7 +1,23 @@
 import { Beach } from '@src/models/beach';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Beaches Functional test', () => {
+  const defaultUser = {
+    name: 'John',
+    email: 'john@mail.com',
+    password: '1234',
+  };
+
+  let token: string;
   beforeAll(async () => await Beach.deleteMany({}));
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+    await User.deleteMany({});
+
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
+  });
   describe('When creating a beach ', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -13,6 +29,7 @@ describe('Beaches Functional test', () => {
 
       const { body, status } = await global.testRequest
         .post('/beaches')
+        .set({ 'x-access-token': token })
         .send(newBeach);
       expect(status).toBe(201);
 
@@ -29,6 +46,7 @@ describe('Beaches Functional test', () => {
 
       const { body, status } = await global.testRequest
         .post('/beaches')
+        .set({ 'x-access-token': token })
         .send(newBeach);
       expect(status).toBe(422);
 
